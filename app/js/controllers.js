eb08bdf56982c6cd4ba3d168421c68ca9da1d920
yunx14 <yunx14@gmail.com>
2014-05-30 12:40:07 -0700

    first commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,13 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
+  .controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+  	$http.get('json/people.json').success(function(data) {
+  		$scope.people = data;
+  	})
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
+  .controller('MyCtrl2', ['$scope', '$http', function($scope, $http) {
+  	$http.get('json/met.json').success(function(data) {
+  		$scope.metrics = data;
+  	})
   }]);
