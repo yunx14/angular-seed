9e9e08f1960569971efb2a5eb493b5349132d8e2
yunx14 <yunx14@gmail.com>
2014-06-02 10:46:08 -0700

    partial 2

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,8 +2,23 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
+angular.module('myApp.filters', [])
+  /*
+  .filter('unique', function() {
+  	return function(input, key) {
+  		var unique = {};
+  		var uniqueList = [];
+  		for (var i=0; i < input.length; i++) {
+  			if (typeof unique[input[i][key]] == 'undefined') {
+  				unique[input[i][key]] = '';
+  				uniqueList.push(input[i]);
+  			}
+  		}
+  		return uniqueList;
+  	}
+  })
+  */
+  .filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     };
